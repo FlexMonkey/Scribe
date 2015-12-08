ef090f6680d43882709c5a6d751d1d7e8032cea4
Simon Gladman <sgladman@gmail.com>
2015-12-08 18:38:01 +0000

    working demo

--- PatternRecognition/ViewController.swift
+++ PatternRecognition/ViewController.swift
@@ -10,152 +10,40 @@ import UIKit
 
 class ViewController: UIViewController
 {
-    let shapeLayer = CAShapeLayer()
-    let cellCount = 8
+    let scribeView = ScribeView()
+    let label = UILabel()
+    
+    var string = ""
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
         
-        shapeLayer.fillColor = nil
-        shapeLayer.strokeColor = UIColor.blackColor().CGColor
-        shapeLayer.lineWidth = 2
+        scribeView.delegate = self
         
-        view.layer.addSublayer(shapeLayer)
-    }
-
-    var strokePoints = [CGPoint]()
-    
-    var minX = CGFloat.max
-    var minY = CGFloat.max
-    var maxX = CGFloat(0.0)
-    var maxY = CGFloat(0.0)
-    
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?)
-    {
-        guard let touch = touches.first else
-        {
-            return
-        }
+        label.font = UIFont.systemFontOfSize(72)
+        label.adjustsFontSizeToFitWidth = true
+        label.textAlignment = .Center
         
-        strokePoints = [touch.locationInView(self.view)]
-        
-        minX = CGFloat.max
-        minY = CGFloat.max
-        maxX = CGFloat(0.0)
-        maxY = CGFloat(0.0)
+        view.addSubview(label)
+        view.addSubview(scribeView)
     }
     
-    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?)
+    override func viewDidLayoutSubviews()
     {
-        guard let
-            touch = touches.first,
-            coalescedTouches = event?.coalescedTouchesForTouch(touch) else
-        {
-            return
-        }
-        
-        for touch in coalescedTouches
-        {
-            let locationInView = touch.locationInView(view)
-            
-            strokePoints.append(locationInView)
-            
-            minX = min(locationInView.x, minX)
-            minY = min(locationInView.y, minY)
-            
-            maxX = max(locationInView.x, maxX)
-            maxY = max(locationInView.y, maxY)
-        }
-        
-        let bezierPath = UIBezierPath()
-        
-        bezierPath.moveToPoint(strokePoints.first!)
-        
-        for point in strokePoints
-        {
-            bezierPath.addLineToPoint(point)
-        }
-        
-        
-        
-        shapeLayer.path = bezierPath.CGPath
+        label.frame = view.bounds
+        scribeView.frame = view.bounds
     }
-    
-    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?)
-    {
-        let gestureWidth = abs(minX - maxX)
-        let gestureHeight = abs(minY - maxY)
-        
-        let cellWidth = max(gestureWidth / CGFloat(cellCount - 1), 1)
-        let cellHeight = max(gestureHeight / CGFloat(cellCount - 1), 1)
-        
-        var cells = [[Bool]](count: cellCount, repeatedValue: [Bool](count: cellCount, repeatedValue: false))
-        
-        let origin = CGPoint(x: min(minX, maxX), y: min(minY, maxY))
-        
-        for point in strokePoints
-        {
-            let x = max(Int(round((point.x - origin.x) / cellWidth)), 0)
-            let y = max(Int(round((point.y - origin.y) / cellHeight)), 0)
-   
-            cells[x][y] = true
-        }
-        
-//        for var i = 0 ; i < cellCount ; i++
-//        {
-//            var foo = ""
-//            
-//            for var j = 0 ; j < cellCount ; j++
-//            {
-//                foo += (cells[j][i] ? "◼︎" : "◻︎")
-//            }
-//            
-//            print(foo)
-//        }
-
-        let result = cells.flatMap({ return $0 }).reduce(UInt64(0))
-        {
-            ($0 << 1 | ($1 ? 1 : 0))
-        }
-        
-        //print("patterns[\(result)] = ")
-        
-        var bestGuess = (UInt64(0), "")
  
-        
-        for (pattern, letter) in patterns.patterns
-        {
-            let result = (pattern & result).popcount()
-            
-            if result > bestGuess.0
-            {
-               bestGuess = (result, letter)
-            }
-        }
-  
-       print("BEST MATCH: ", bestGuess)
-        
-    }
-    
-    var patterns = Patterns()
-
 }
 
-extension UInt64
+extension ViewController: ScribeViewDelegate
 {
-    func popcount() -> UInt64
+    func scribeView(scribeView: ScribeView, didMatchPattern: String)
     {
-        var copy = self
-        var popcount = UInt64(0)
+        string += didMatchPattern
         
-        while copy > 0
-        {
-            popcount += copy & 1
-            copy >>= 1
-        }
-        
-        return popcount
+        label.text = string
     }
 }
 
---
+++ PatternRecognition/scribe/ScribeView.swift
@@ -0,0 +1,213 @@
+//
+//  ScribeView.swift
+//  PatternRecognition
+//
+//  Created by Simon Gladman on 08/12/2015.
+//  Copyright © 2015 Simon Gladman. All rights reserved.
+//
+
+import UIKit
+
+
+class ScribeView: UIView
+{
+    let patterns = Patterns().patterns
+    let bezierPath = UIBezierPath()
+    let shapeLayer = CAShapeLayer()
+    let cellCount = 8
+    
+    weak var delegate: ScribeViewDelegate?
+    
+    var strokePoints = [CGPoint]()
+    
+    var minX = CGFloat.max
+    var minY = CGFloat.max
+    var maxX = CGFloat(0.0)
+    var maxY = CGFloat(0.0)
+    
+    var timer: NSTimer?
+    
+    var inflight = false
+    {
+        didSet
+        {
+            if inflight
+            {
+                timer?.invalidate()
+            }
+            
+            UIView.animateWithDuration(0.15)
+            {
+                self.backgroundColor = self.inflight ? UIColor(white: 0.5, alpha: 0.5) : nil
+            }
+        }
+    }
+
+    override init(frame: CGRect)
+    {
+        super.init(frame: frame)
+        
+        shapeLayer.fillColor = nil
+        shapeLayer.strokeColor = UIColor.blackColor().CGColor
+        shapeLayer.lineWidth = 2
+        
+        layer.addSublayer(shapeLayer)
+    }
+
+    required init?(coder aDecoder: NSCoder)
+    {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?)
+    {
+        guard let touch = touches.first else
+        {
+            return
+        }
+        
+        if !inflight
+        {
+            strokePoints = [touch.locationInView(self)]
+            
+            minX = CGFloat.max
+            minY = CGFloat.max
+            maxX = CGFloat(0.0)
+            maxY = CGFloat(0.0)
+            
+            bezierPath.removeAllPoints()
+        }
+        
+        bezierPath.moveToPoint(touch.locationInView(self))
+        inflight = true
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?)
+    {
+        guard let
+            touch = touches.first,
+            coalescedTouches = event?.coalescedTouchesForTouch(touch) else
+        {
+            return
+        }
+        
+        inflight = true
+        
+        for touch in coalescedTouches
+        {
+            let locationInView = touch.locationInView(self)
+            
+            strokePoints.append(locationInView)
+            bezierPath.addLineToPoint(locationInView)
+            
+            minX = min(locationInView.x, minX)
+            minY = min(locationInView.y, minY)
+            
+            maxX = max(locationInView.x, maxX)
+            maxY = max(locationInView.y, maxY)
+        }
+        
+        shapeLayer.path = bezierPath.CGPath
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?)
+    {
+        timer =  NSTimer.scheduledTimerWithTimeInterval(0.25,
+            target: self,
+            selector: "timerHandler",
+            userInfo: nil,
+            repeats: false)
+    }
+    
+    func timerHandler()
+    {
+        handleGesture()
+        
+        inflight = false
+        
+        bezierPath.removeAllPoints()
+        shapeLayer.path = bezierPath.CGPath
+    }
+    
+    func handleGesture()
+    {
+        let gestureWidth = abs(minX - maxX)
+        let gestureHeight = abs(minY - maxY)
+        
+        let cellWidth = max(gestureWidth / CGFloat(cellCount - 1), 1)
+        let cellHeight = max(gestureHeight / CGFloat(cellCount - 1), 1)
+        
+        var cells = [[Bool]](count: cellCount, repeatedValue: [Bool](count: cellCount, repeatedValue: false))
+        
+        let origin = CGPoint(x: min(minX, maxX), y: min(minY, maxY))
+        
+        for point in strokePoints
+        {
+            let x = max(Int(round((point.x - origin.x) / cellWidth)), 0)
+            let y = max(Int(round((point.y - origin.y) / cellHeight)), 0)
+            
+            cells[x][y] = true
+        }
+        
+        printToConsole(cells)
+        
+        let strokeResult = cells.flatMap({ return $0 }).reduce(UInt64(0))
+        {
+            ($0 << 1 | ($1 ? 1 : 0))
+        }
+        
+        let bestGuess = patterns.reduce((UInt64(0), ""))
+        {
+            let popcount = ($1.0 & strokeResult).popcount()
+            
+            return popcount > $0.0 ? (popcount, $1.1) : $0
+        }
+        
+        delegate?.scribeView(self, didMatchPattern: bestGuess.1)
+    }
+    
+    func printToConsole(cells: [[Bool]])
+    {
+        print("")
+        
+        for var i = 0 ; i < cellCount ; i++
+        {
+            var row = ""
+            
+            for var j = 0 ; j < cellCount ; j++
+            {
+                row += (cells[j][i] ? "◼︎" : "◻︎")
+            }
+            
+            print(row)
+        }
+        
+        print("")
+    }
+}
+
+// MARK: ScribeViewDelegate protocol
+
+protocol ScribeViewDelegate: class
+{
+    func scribeView(scribeView: ScribeView, didMatchPattern: String)
+}
+
+// MARK: popcount() extension for UInt64
+
+extension UInt64
+{
+    func popcount() -> UInt64
+    {
+        var copy = self
+        var popcount = UInt64(0)
+        
+        while copy > 0
+        {
+            popcount += copy & 1
+            copy >>= 1
+        }
+        
+        return popcount
+    }
+}
